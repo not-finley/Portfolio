c8a36a724496db4b57ed1ee533eaeb2b1014ff5c
Finley <finley.harrison2019@gmail.com>
2024-12-29 15:02:05 -0500

    bug fix mobile

--- src/pages/Artwork.tsx
+++ src/pages/Artwork.tsx
@@ -15,7 +15,7 @@ const Artwork = () => {
       slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 767, min: 464 },
+      breakpoint: { max: 767, min: 0 },
       items: 1,
       slidesToSlide: 1 // optional, default to 1.
     }
