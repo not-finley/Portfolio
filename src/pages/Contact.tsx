b20c7eee9048e9943ea3cd3679c20a1ecb8602db
Finley Harrison <fharriso@uwaterloo.ca>
2024-12-30 11:37:49 -0500

    contact changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import emailjs from '@emailjs/browser';
-import { BiLoader } from "react-icons/bi";
+import { CircularLoading, SpinLoading } from 'respinner';
+import { Slide, ToastContainer, toast } from 'react-toastify';
 
 const Contact = () => {
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
@@ -10,14 +11,25 @@ const Contact = () => {
         setIsSubmitting(true);
 
         emailjs.sendForm(
-            "service_4kp24nw",
-            "template_jscxwwh",
-            e.currentTarget,
+            `service_4kp24nw`,
+            `template_jscxwwh`,
+            e.currentTarget, 
             "9ZIgVLaQhluUkMCVL"
         ).then(
             (result) => {
                 console.log(result);
                 setIsSubmitting(false);
+                return(toast.success("Message Sent!", {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Slide,
+                    }))
             },
             (error) => {
                 console.log(error);
@@ -33,7 +45,7 @@ const Contact = () => {
         <div id="contact" className="bg-blue-950 h-auto py-10 px-4">
             <div className="max-w-4xl mx-auto text-center mb-28">
                 <h1 className="text-4xl font-bold text-white m-10">Contact Me</h1>
-                <p className="text-gray-400 mb-4">
+                <p className="text-gray-200 mb-4">
                     Feel free to reach out if you're interested in working together or just want to say hi!
                 </p>
                 <form className="space-y-4" onSubmit={sendEmail}>
@@ -48,20 +60,36 @@ const Contact = () => {
                         name="user_email"
                         placeholder="Your Email"
                         className="w-full p-3 border rounded-lg bg-blue-900"
+                        required
+                        title="Please enter a valid email address."
                     />
                     <textarea
                         name="message"
                         placeholder="Your Message"
                         className="w-full p-3 border rounded-lg bg-blue-900"
                         rows={5}
+                        required
                     ></textarea>
                     <button
                         type="submit"
                         disabled={isSubmitting}
                         className="bg-yellow-500 text-white px-6 py-3 rounded-lg hover:bg-yellow-400"
                     >
-                        {isSubmitting?<BiLoader/>:'Send'}
+                        {isSubmitting ? <SpinLoading fill="white" borderRadius={4} count={12}/> : 'Send'}
                     </button>
+                    <ToastContainer
+                        position="bottom-right"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="dark"
+                        transition={Slide}
+                    />
                 </form>
             </div>
         </div>
